28d8d704596d0eb195978b1088a8b498e6c6f119
Abdelrahman Zaytoun <abdelrahmanzaytounn@gmail.com>
2022-02-25 15:57:42 -0500

    update app logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
 import "./App.css";
+import { Login, Register } from "./components/Login";
+import { Header } from "./components/Header";
+import { AddPet, ListPet } from "./components/Pet";
 
 function App() {
-  return (<div></div>)
+  const token = localStorage.getItem("token");
+  const [toggleLogin, setToggleLogin] = useState(true);
+  const [isLogged, setIsLogged] = useState(!!token);
+  const [isListLayout, setIsListLayout] = useState(true);
+  const logout = () => {
+    localStorage.removeItem("token");
+    setIsLogged(false);
+  };
+  return isLogged ? (
+    <div>
+      <Header
+        logout={logout}
+        isListLayout={(isList) => setIsListLayout(isList)}
+      />
+      {isListLayout ? (
+        <ListPet />
+      ) : (
+        <AddPet isListLayout={() => setIsListLayout(true)} />
+      )}
+    </div>
+  ) : (
+    <div className="App">
+      {toggleLogin ? (
+        <Login
+          toggleLogin={() => setToggleLogin(!toggleLogin)}
+          isLogged={() => setIsLogged(true)}
+        />
+      ) : (
+        <Register
+          toggleLogin={() => setToggleLogin(!toggleLogin)}
+          isLogged={() => setIsLogged(true)}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
